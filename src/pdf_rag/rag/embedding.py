3f187c137bb98cf3ae862fc5652b5f339f29e945
Seita Fujiwara <seitafujiwara@gmail.com>
2025-12-29 07:02:17 +0100

    feat: embedding query and generate ans

--- src/pdf_rag/rag/embedding.py
+++ src/pdf_rag/rag/embedding.py
@@ -3,7 +3,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-def embedding_text(extracted_text, filename):
+def embedding_data(extracted_text, filename):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
@@ -21,3 +21,12 @@ def embedding_text(extracted_text, filename):
     )
 
     return db
+
+
+def search_similar_documents(query: str, k: int = 3):
+    # Embedding user input
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    db = Chroma(persist_directory="data/chroma_db", embedding_function=embeddings)
+
+    results = db.similarity_search_with_relevance_scores(query, k=k)
+    return results
